52184bdea92228b861dd20f6f4ddc5f3c3300f20
Arpit Jaswal <iamarpitjaswal@gmail.com>
2022-07-21 15:23:20 +0530

    Time: 6 ms (23.76%), Space: 8.3 MB (23.39%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        //bubble sort
+        //bubble sort(optimised)
         int n=nums.size();
         for(int i=0;i<n-1;i++){
+            bool swapped=false;
             for(int j=0;j<n-i-1;j++){
-                if(nums[j]>nums[j+1])swap(nums[j],nums[j+1]);
+                if(nums[j]>nums[j+1]){
+                    swap(nums[j],nums[j+1]);
+                    swapped=true;
+                }
             }
+            if(swapped==false)break;
         }
     }
 };
