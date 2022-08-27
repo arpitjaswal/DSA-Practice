62d9dcd7691cb816bcf1aca29d1a158e11385cc8
Arpit Jaswal <iamarpitjaswal@gmail.com>
2022-08-27 12:15:01 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (68.93%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -1,15 +1,24 @@
 class Solution {
-private:
-    string sortusingstring(int n){
-        string s=to_string(n);
-        sort(s.begin(), s.end());
-        return s;
-    }
+// private:
+//     string sortusingstring(int n){
+//         string s=to_string(n);
+//         sort(s.begin(), s.end());
+//         return s;
+//     }
 public:
     bool reorderedPowerOf2(int n) {
-        string s=sortusingstring(n);
-        for(int i=0;i<32;i++){
-            if(s==sortusingstring(1<<i))return true;
+//         string s=sortusingstring(n);
+//         for(int i=0;i<32;i++){
+//             if(s==sortusingstring(1<<i))return true;
+//         }
+//         return false;
+        string s=to_string(n);
+        sort(s.begin(), s.end());
+        for(int i=0;i<31;i++){
+            int n=pow(2,i);
+            string a=to_string(n);
+            sort(a.begin(),a.end());
+            if(s==a)return true;
         }
         return false;
     }
