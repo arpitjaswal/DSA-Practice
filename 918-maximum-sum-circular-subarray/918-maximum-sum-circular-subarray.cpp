34933615bcd11890b7c527aa8a5ad58b88dd0bd8
Arpit Jaswal <iamarpitjaswal@gmail.com>
2022-09-24 16:49:08 +0530

    Time: 123 ms (40.53%), Space: 40 MB (32.73%) - LeetHub

--- 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.cpp
+++ 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     int maxSubarraySumCircular(vector<int>& nums) {
-        int curMax=0,curMin=0,n=nums.size(),total=0,MaxSum=nums[0],MinSum=nums[0];
-        for(auto a:nums){
-            total+=a;
-            curMin=min(curMin+a,a);
-            MinSum=min(MinSum,curMin);
-            curMax=max(curMax+a,a);
-            MaxSum=max(MaxSum,curMax);
+        //intialise current max and overall max
+        //initialise current min and overall min
+        //keep track of total sum upuntil
+        //keep adding to curmin and curmax and keep checking max/min of their values
+        //at the end, if maxsum is positive, return max of maxsum and total-minsum else just return maxsum
+        int curmin=0,curmax=0,maxsum=nums[0],minsum=nums[0],n=nums.size(),total=0;
+        for(auto c: nums){
+            total+=c;
+            curmin=min(curmin+c,c);
+            minsum=min(minsum,curmin);
+            curmax=max(curmax+c,c);
+            maxsum=max(maxsum,curmax);
         }
-        return MaxSum>0?max(MaxSum,total-MinSum):MaxSum;
+        return maxsum>0?max(maxsum, total-minsum):maxsum;
     }
 };
