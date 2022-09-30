157e3e369d68526c9b6e3035ebafed9bc98b6f86
Arpit Jaswal <iamarpitjaswal@gmail.com>
2022-09-30 16:19:16 +0530

    Time: 20 ms (42.89%), Space: 7.4 MB (13.17%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int>m;
+        for(auto x: s){
+            m[x]++;
+        }
+        for(auto x: t){
+            m[x]--;
+        }
+        for(auto x: m){
+            if(x.second!=0)return false;
+        }
+        return true;
+    }
+};
