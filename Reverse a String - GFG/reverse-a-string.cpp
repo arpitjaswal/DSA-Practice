36176004e9945f3444bab80073d81e6f23c79810
Arpit Jaswal <iamarpitjaswal@gmail.com>
2023-08-02 05:17:29 +0530

    Added solution - LeetHub

--- Reverse a String - GFG/reverse-a-string.cpp
+++ Reverse a String - GFG/reverse-a-string.cpp
@@ -1,48 +1,35 @@
-// { Driver Code Starts
-//Initial Template for C++
-
-
-#include<bits/stdc++.h>
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
 using namespace std;
 
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    string revStr(string S) {
+        int s = 0;
+        int e = S.length()-1;
+        while(s<=e){
+            char temp = S[s];
+            S[s++]=S[e];
+            S[e--]=temp;
+        }
+        return S;
+    }
+};
 
-string reverseWord(string str);
-
-
+//{ Driver Code Starts.
 int main() {
-	
-	int t;
-	cin>>t;
-	while(t--)
-	{
-	string s;
-	cin >> s;
-	
-	cout << reverseWord(s) << endl;
-	}
-	return 0;
-	
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        cin >> S;
+        Solution ob;
+        cout << ob.revStr(S) << endl;
+    }
+    return 0;
 }
 
 // } Driver Code Ends
-
-
-//User function Template for C++
-void helper(string &str, int i, int j){
-    //base case
-    if(i>=j)return;
-    
-    //processing
-    char temp=str[i];
-    str[i]=str[j];
-    str[j]=temp;
-    
-    //recursive call
-    helper(str,++i,--j);
-}
-string reverseWord(string str){
-    int i=0;
-    int j=str.length()-1;
-    helper(str,i,j);
-    return str;
-}
